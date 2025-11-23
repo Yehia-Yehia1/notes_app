5670cc84da820c59226d0d6f689f7ab1b807bee6
YousifMahmoud16 <yousifmahmoud465@gmail.com>
2025-11-23 21:50:14 +0200

    create edit note view

---
+++ app/lib/views/edit_note_view.dart
@@ -0,0 +1,12 @@
+import 'package:app/views/widgets/edit_view_body.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class EditNoteView extends StatelessWidget {
+  const EditNoteView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: EditNoteViewBody());
+  }
+}
--- app/lib/views/widgets/custom_note_item.dart
+++ app/lib/views/widgets/custom_note_item.dart
@@ -1,3 +1,4 @@
+import 'package:app/views/edit_note_view.dart';
 import 'package:flutter/material.dart';
 
 class NoteItem extends StatelessWidget {
@@ -5,46 +6,58 @@ class NoteItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffFFCC80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffFFCC80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16, bottom: 16),
+                child: Text(
+                  'My fist app',
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(.4),
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.delete, color: Colors.black, size: 24),
+              ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16, bottom: 16),
+            Padding(
+              padding: const EdgeInsets.only(right: 24),
               child: Text(
-                'My fist app',
+                'November19 , 2025',
                 style: TextStyle(
                   color: Colors.black.withOpacity(.4),
-                  fontSize: 18,
+                  fontSize: 16,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.delete, color: Colors.black, size: 24),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24),
-            child: Text(
-              'November19 , 2025',
-              style: TextStyle(
-                color: Colors.black.withOpacity(.4),
-                fontSize: 16,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
